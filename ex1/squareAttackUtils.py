38d1c93d0651106e2008b9b2ee50161beab3d001
Matej Majtan <majtan.matej@gmail.com>
2022-02-16 01:36:43 +0100

    Finished the Square attack

--- ex1/squareAttackUtils.py
+++ ex1/squareAttackUtils.py
@@ -1,62 +1,131 @@
-from aes import SI, encrypt
+import random
+from aes import SI, encrypt, r_con, subWord, rotWord, Nk, Nb, Nr
 
-numberOfDeltaSets= 5
-defaultKey = [ 0x2b, 0x7e, 0x15, 0x16,
-        0x28, 0xae, 0xd2, 0xa6,
-        0xab, 0xf7, 0x15, 0x88,
-        0x09, 0xcf, 0x4f, 0x3c]
-
-def generateDeltaSet(randomValue=0,chosenIndex=0):
-    # Provide randomValue if it is 
-    # needed in the non active indexes
-    stateTemplate = [randomValue] * 16
+# Prepares a delta set
+# randomValue - initial value in the positions besides the chosen index
+# chosenIndex - index at which the values with the XOR property are written to
+def generateDeltaSet(randomValue=0, chosenIndex=0):
     deltaSet = []
     for index in range(256):
-        currentState = stateTemplate.copy()
+        currentState = [randomValue] * 16
         currentState[chosenIndex] = index
         deltaSet.append(currentState)
     return deltaSet
 
-def encryptDeltaSet(deltaSet,key):
+# Encrypts delta set
+def encryptDeltaSet(deltaSet, key):
+    # apply encryption with provided key on each of the states in delta set
     return [encrypt(state,key) for state in deltaSet]
 
-def reverseGuessPosition(guess,chosenIndex,deltaState):
-    return SI[deltaState[chosenIndex]^guess]
+# Reverse the value of a byte using given guess and position
+def reverseGuessPositionValue(guess, position, deltaState):
+    # remove guess from the value at the position and invert the S-box
+    return SI[deltaState[position]^guess]
 
-def guessKeyValues(guess,chosenIndex,encryptedDeltaSet):
+# Verify a guess at given position
+def verifyGuessPosition(guess, position, encryptedDeltaSet):
     result= 0
-    for eachSet in encryptedDeltaSet:
-        result ^= reverseGuessPosition(guess,chosenIndex,eachSet) 
+    # loop through all states in delta set and verify that the XOR property holds for given guess
+    for deltaState in encryptedDeltaSet:
+        result ^= reverseGuessPositionValue(guess, position, deltaState) 
     return result == 0
-    
-def checkAllGuesses(chosenIndex,encryptedDeltaSet):
+
+# Finds all possible guesses for given position in the last round key
+# position - position in the last round key to be guessed
+# encryptedDeltaSets - list of delta sets encrypted by the key to be guessed    
+def checkAllGuesses(position, encryptedDeltaSet):
     correctGuesses = []
+    # go through all possible guesses and append it to the result if the guess is correct
     for guess in range(256):
-        if guessKeyValues(guess,chosenIndex,encryptedDeltaSet):
+        if verifyGuessPosition(guess, position, encryptedDeltaSet):
             correctGuesses.append(guess)
     return correctGuesses
 
-def getFinalGuess(chosenIndex,encryptedDeltaSets):
-    correctGuesses = checkAllGuesses(chosenIndex, encryptedDeltaSets[0])
+# Finds the correct guess for given position in the last round key
+# position - position in the last round key to be guessed
+# encryptedDeltaSets - list of delta sets encrypted by the key to be guessed
+def getFinalGuess(position, encryptedDeltaSets):
+    # get all the guesses for the first delta set
+    correctGuesses = checkAllGuesses(position, encryptedDeltaSets[0])
+
+    # check for the rest of delta sets
     for idx in range(1,len(encryptedDeltaSets)):
+        # in case that there is only one guess return
         if len(correctGuesses)==1:
             return correctGuesses[0]
-        cg = checkAllGuesses(chosenIndex,encryptedDeltaSets[idx])
+        # check guesses for next delta set
+        cg = checkAllGuesses(position, encryptedDeltaSets[idx])
+        # find intersection of the previous guesses and the new ones
+        # this should reduce the set of initial guesses to one value
         correctGuesses=[value for value in cg if value in correctGuesses]
+    # in the case that the delta sets do not reduce the guesses to only one guess
+    # then the function throws an exception
     raise Exception("couldn't find final guess")
 
-def determineLastRoundKey(encryptedDeltaSets):
+# Helper function reversing word order of 4-word lists
+# wordList - 16-byte list
+def reverseWordListOrder(wordList):
+    result = []
+
+    # loop in reverse through each word and copy the bytes in correct order
+    for i in reversed(range(4)):
+        result.append(wordList[4 * i])
+        result.append(wordList[4 * i + 1])
+        result.append(wordList[4 * i + 2])
+        result.append(wordList[4 * i + 3])
+
+    return result
+
+# lastRoundKey is 4 byte list
+# reverseExpandedKey (Nr + 1) keys * 16 bytes = 80 bytes
+def invertKeySchedule(lastRoundKey):
+    # put the last round key into the reverse expanded key buffer in reverse word order
+    reverseExpandedKey = reverseWordListOrder(lastRoundKey)
+
+    # loop through all 4 words in each round (4x4 times)
+    for i in range(Nk, Nb * (Nr + 1)):
+        # figure out indices for word which is 4-words ahead and 3-words ahead
+        prevRoundIdx = 4 * (i - Nk)
+        nextPrevRoundIdx = prevRoundIdx + Nk
+        # get the words for the given indices
+        prevRound = [reverseExpandedKey[prevRoundIdx], reverseExpandedKey[prevRoundIdx + 1], reverseExpandedKey[prevRoundIdx + 2], reverseExpandedKey[prevRoundIdx + 3]]
+        temp = [reverseExpandedKey[nextPrevRoundIdx], reverseExpandedKey[nextPrevRoundIdx + 1], reverseExpandedKey[nextPrevRoundIdx + 2], reverseExpandedKey[nextPrevRoundIdx + 3]]
+
+        # if it is the start word (since the buffer is reversed it is the last one in round key) apply the diffusion algorithm on the 3-words ahead word 
+        if i % Nk == 3:
+            temp = subWord(rotWord(temp))
+            # the r_con index also needs to be inverted hence number of rounds - current round
+            temp[3] ^= r_con[(Nk + 1) - (i / Nk)]
+
+        # revert the final XOR and append the result to the inverted expanded key buffer
+        for j in range(0, 4):
+            reverseExpandedKey.append(temp[j] ^ prevRound[j])
+
+    # return the last 4 words (representing the key) in reverse word order
+    return reverseWordListOrder(reverseExpandedKey[-16:])
+
+# Key guessing function
+# encrypredDeltaSets - list of delta sets encrypted by the key to be guessed
+def crack(encryptedDeltaSets):
     lastRoundKey= []
+
+    # guess each key byte separately
     for position in range(16):
-        lastRoundKey.append(getFinalGuess(position,encryptedDeltaSets))
-    return lastRoundKey
+        lastRoundKey.append(getFinalGuess(position, encryptedDeltaSets))
+    
+    # return original key
+    return invertKeySchedule(lastRoundKey)
 
-def invertKeySchedule():
-    return "Done"
+if __name__ == "__main__":
+    # test key randomly generated
+    keyToGuess = [random.randint(0x00, 0xFF) for _ in range(16)]
 
-def crack(key=defaultKey):
-    encryptedDeltaSets=[]
-    for index in range(numberOfDeltaSets):
-        encryptedDeltaSets.append(encrypt(generateDeltaSet,key))
-    lastRoundKey = determineLastRoundKey(encryptedDeltaSets)
-    return invertKeySchedule()
+    # four delta sets used to crack the key
+    deltaSets = [generateDeltaSet(0x00), generateDeltaSet(0x12), generateDeltaSet(0x34), generateDeltaSet(0xFF)]
+    # encrypted delta sets (by the test key) used for cracking
+    encryptedDeltaSets = map(lambda ds: encryptDeltaSet(ds, keyToGuess), deltaSets)
+
+    guessedKey = crack(encryptedDeltaSets)
+
+    print("Key to guess:", keyToGuess)
+    print("Guessed key:", guessedKey)
